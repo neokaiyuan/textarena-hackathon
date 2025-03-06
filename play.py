18c18c51466d00f970f2724013a10fcf3b65321b
Neo Kai Yuan <3345016+neokaiyuan@users.noreply.github.com>
2025-03-06 22:35:07 +0800

    Initial commit to play game

---
+++ play.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import textarena as ta
+
+# Initialize agents
+agents = {
+    0: ta.agents.OpenRouterAgent(model_name="GPT-4o-mini"),
+    1: ta.agents.OpenRouterAgent(model_name="anthropic/claude-3.5-haiku"),
+}
+
+# Initialize environment from subset and wrap it
+env = ta.make(env_id="BalancedSubset-v0")
+env = ta.wrappers.LLMObservationWrapper(env=env)
+env = ta.wrappers.SimpleRenderWrapper(
+    env=env,
+    player_names={0: "GPT-4o-mini", 1: "claude-3.5-haiku"},
+)
+
+env.reset()
+done = False
+while not done:
+    player_id, observation = env.get_observation()
+    action = agents[player_id](observation)
+    done, info = env.step(action=action)
+rewards = env.close()
