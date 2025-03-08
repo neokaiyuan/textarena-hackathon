06b60d3e820c3eba3dd4d72b5c012aad9196eba1
Neo Kai Yuan <3345016+neokaiyuan@users.noreply.github.com>
2025-03-08 12:18:13 +0800

    Poker working on Bedrock

--- play.py
+++ play.py
@@ -1,23 +1,28 @@
 from dotenv import load_dotenv
+
 load_dotenv()
 
 import textarena as ta
 
 # Initialize agents
 agents = {
-    0: ta.agents.OpenRouterAgent(model_name="GPT-4o-mini"),
-    1: ta.agents.OpenRouterAgent(model_name="anthropic/claude-3.5-haiku"),
+    0: ta.agents.AWSBedrockAgent(
+        model_id="anthropic.claude-3-5-sonnet-20240620-v1:0", region_name="us-west-2"
+    ),
+    1: ta.agents.AWSBedrockAgent(
+        model_id="anthropic.claude-3-5-haiku-20241022-v1:0", region_name="us-west-2"
+    ),
 }
 
 # Initialize environment from subset and wrap it
-env = ta.make(env_id="BalancedSubset-v0")
+env = ta.make(env_id="Poker-v0")
 env = ta.wrappers.LLMObservationWrapper(env=env)
 env = ta.wrappers.SimpleRenderWrapper(
     env=env,
-    player_names={0: "GPT-4o-mini", 1: "claude-3.5-haiku"},
+    player_names={0: "sonnet", 1: "haiku"},
 )
 
-env.reset()
+env.reset(num_players=len(agents))
 done = False
 while not done:
     player_id, observation = env.get_observation()
